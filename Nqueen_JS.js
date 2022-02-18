f1f3acc60a3736c9e7a3eebc8c69e75fe7e08f55
Sam Nick <72904070+SamNickGammer@users.noreply.github.com>
2022-02-19 00:36:44 +0530

    Updated..........

--- Nqueen_JS.js
+++ Nqueen_JS.js
@@ -1,94 +1,165 @@
-function queenPuzzle(rows, columns) {
-  if (rows <= 0) {
-    return [[]];
-  } else {
-    return addQueen(rows - 1, columns);
-  }
-}
-
-function addQueen(newRow, columns) {
-  var newSolutions = [];
-  var prev = queenPuzzle(newRow, columns);
-  for (var i = 0; i < prev.length; i++) {
-    var solution = prev[i];
-    for (var newColumn = 0; newColumn < columns; newColumn++) {
-      if (!hasConflict(newRow, newColumn, solution))
-        newSolutions.push(solution.concat([newColumn]));
-    }
-  }
-  return newSolutions;
-}
-
-function hasConflict(newRow, newColumn, solution) {
-  for (var i = 0; i < newRow; i++) {
-    if (
-      solution[i] == newColumn ||
-      solution[i] + i == newColumn + newRow ||
-      solution[i] - i == newColumn - newRow
-    ) {
-      return true;
-    }
-  }
-  return false;
-}
-
 const imagesCont = document.querySelector('.imagesCont');
-const rowEle = document.getElementById('rows');
+const rowInput = document.querySelector('#rows');
 const submit = document.querySelector('.submitBtn');
+const board = document.querySelector('#board');
+const squareSize = 70;
+
+var b = false;
+var boardArr = [[]];
+var rows;
+var count = 0;
+var count1 = 0;
+
+rowInput.onkeyup = function (e) {
+  removeAllChildNodes(board);
+  b = false;
+  count = 0;
+  count1 = 0;
+};
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
 
 submit.addEventListener('click', (e) => {
-  var rows = rowEle.value;
-  var columns = rowEle.value;
-  var color = 1;
-  imagesCont.innerHTML = '';
-
-  var queens = queenPuzzle(rows, columns);
-  console.log(queens);
-  //   var queens = [1, 2];
-  queens.forEach((queen) => {
-    var print_board = function (columns) {
-      var n = columns.length,
-        row = 0,
-        col = 0;
-
-      var divI = document.createElement('div');
-      divI.className = 'images';
-      imagesCont.appendChild(divI);
-
-      while (row < n) {
-        var divC = document.createElement('div');
-        divC.className = 'row';
-        divI.appendChild(divC);
-
-        while (col < n) {
-          var divP = document.createElement('div');
-          divP.className = 'pice';
-          divC.appendChild(divP);
-
-          if (color % 2 == 0) {
-            divP.classList.add('black');
-          } else {
-            divP.classList.add('white');
-          }
-          color++;
-          //   (columns[row] === col ? 'Q ' : '# ');
-          if (columns[row] == col) {
-            divP.innerHTML = `<div class="queenPosition correctPosition">
-              <i class="fas fa-chess-queen"></i>
-           </div>`;
-          }
-
-          col++;
-        }
-
-        if (rowEle.value % 2 == 0) {
-          color += 1;
-        }
-        // process.stdout.write('\n');
-        col = 0;
-        row++;
-      }
-    };
-    print_board(queen);
-  });
+  rows = rowInput.value;
+  if (!b) {
+    printBoard(rows);
+    b = true;
+  }
+  boardArr = new Array(rows);
+  for (var i = 0; i < rows; i++) {
+    boardArr[i] = new Array(rows);
+    for (var j = 0; j < rows; j++) {
+      boardArr[i][j] = '0';
+      console.log(boardArr);
+    }
+  }
+  //   boardArr = [
+  //     [1, 1, 0, 0],
+  //     [0, 1, 1, 0],
+  //     [1, 1, 1, 1],
+  //     [0, 1, 1, 1],
+  //   ];
+  printArray();
 });
+
+function printBoard(inputRows) {
+  board.style.width = inputRows * (squareSize + 2) + 'px';
+  for (var i = 0; i < inputRows; i++) {
+    for (var j = 0; j < inputRows; j++) {
+      count++;
+      count1++;
+      var square = document.createElement('div');
+      square.id = 'square' + count;
+      square.style.height = squareSize + 'px';
+      square.style.width = squareSize + 'px';
+      square.style.border = '1px solid black';
+      if (count1 % 2 == 0) {
+        square.style.backgroundColor = 'white';
+      } else {
+        square.style.backgroundColor = 'gray';
+      }
+      board.appendChild(square);
+      square.appendChild(queen('queen' + count));
+      square.onclick = function (e) {
+        var idd = e.target.id.replace(/\D/g, '');
+        var tempQueen = document.querySelector('#queen' + idd);
+        if (tempQueen.style.visibility == 'hidden') {
+          tempQueen.style.visibility = 'visible';
+          boardArr[getCol(idd, inputRows)][getRow(idd, inputRows)] = 1;
+          // checkForChecks(getCol(idd, inputRows), getRow(idd, inputRows));
+        } else {
+          tempQueen.style.visibility = 'hidden';
+          boardArr[getCol(idd, inputRows)][getRow(idd, inputRows)] = 0;
+          // drawBoard();
+        }
+        printArray();
+      };
+    }
+    if (inputRows % 2 == 0) {
+      count1++;
+    }
+  }
+}
+
+function queen(id) {
+  var queenPiece = document.createElement('div');
+  queenPiece.id = id;
+  queenPiece.style.height = squareSize + 'px';
+  queenPiece.style.width = squareSize + 'px';
+  queenPiece.style.background = 'url(queen.svg)';
+  queenPiece.style.backgroundSize = 'cover';
+  queenPiece.style.position = 'absolute';
+  queenPiece.style.visibility = 'hidden';
+  return queenPiece;
+}
+
+function printArray() {
+  checked_boxes = 0;
+  drawBoard();
+  for (var i = 0; i < rows; i++) {
+    for (var j = 0; j < rows; j++) {
+      if (boardArr[i][j] == 1) {
+        checkForChecks(i, j);
+      }
+      if (boardArr[i][j] == 1) {
+        document.querySelector('#queen' + (i * rows + j + 1)).style.visibility =
+          'visible';
+      } else {
+        document.querySelector('#queen' + (i * rows + j + 1)).style.visibility =
+          'hidden';
+      }
+    }
+  }
+  console.log(checked_boxes);
+}
+
+function getRow(inpt, size) {
+  return (inpt - 1) % size;
+}
+function getCol(inpt, size) {
+  return parseInt((inpt - 1) / size, 0);
+}
+var checked_boxes = 0;
+function checkForChecks(i1, j1, check) {
+  for (var i = 0; i < rows; i++) {
+    for (var j = 0; j < rows; j++) {
+      if (i == i1) {
+        checked_boxes++;
+      } else if (j == j1) {
+        checked_boxes++;
+      } else {
+        if (i + (j1 - i1) == j || i + (rows - 1 - j1 - i1) == rows - 1 - j) {
+          checked_boxes++;
+          document.querySelector(
+            '#square' + Number(i * rows + j + 1)
+          ).style.backgroundColor = 'red';
+        }
+      }
+      tempSquareRow = document.querySelector('#square' + (i1 * rows + j + 1)); // 0*4 + 1
+      tempSquareColumn = document.querySelector(
+        '#square' + (i * rows + j1 + 1)
+      );
+      tempSquareRow.style.backgroundColor = 'red';
+      tempSquareColumn.style.backgroundColor = 'red';
+    }
+  }
+}
+
+function drawBoard() {
+  var count2 = 0;
+  for (var i = 1; i <= rows * rows; i++) {
+    count2++;
+    if (count2 % 2 == 0) {
+      document.querySelector('#square' + i).style.backgroundColor = 'white';
+    } else {
+      document.querySelector('#square' + i).style.backgroundColor = 'gray';
+    }
+    if (rows % 2 == 0 && i % rows == 0) {
+      count2++;
+    }
+  }
+}
